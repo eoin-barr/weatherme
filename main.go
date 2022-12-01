5c58c56c51a291a8cd32af0c5157ea5b92810873
Eoin Barr <eoin.barr@ucdconnect.ie>
2022-12-01 19:23:30 +0000

    update env 6

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 /*
 Copyright © 2022 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/eoin-barr/weatherme/cmd"
+import (
+	"log"
+
+	"github.com/eoin-barr/weatherme/cmd"
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	// loads values from .env into the system
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("No .env file found")
+	}
+}
 
 func main() {
 	cmd.Execute()
